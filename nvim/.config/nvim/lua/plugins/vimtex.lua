6a9545a8e3e03813c680dd329d14a4dde2799a0c
Samuel de Araujo Brandao <samuelbaraujo19@gmail.com>
2026-08-31 09:25:01 -0300

    SUPER+Shift+P binding (AoPS), and LuaLatex (didn't work, will fix later)

--- nvim/.config/nvim/lua/plugins/vimtex.lua
+++ nvim/.config/nvim/lua/plugins/vimtex.lua
@@ -4,6 +4,14 @@ return {
   -- tag = "v2.15", -- uncomment to pin to a specific release
   init = function()
     -- VimTeX configuration goes here, e.g.
+    vim.g.vimtex_compiler_latexmk = {
+      options = {
+        '-lualatex',
+      },
+    }
     vim.g.vimtex_view_method = "zathura"
+    vim.g.vimtex_compiler_method = "latexmk"
+    vim.keymap.set("n", "<leader>ll", "<cmd>VimtexCompile<CR>", { desc = "VimTeX Compile" })
+    vim.keymap.set("n", "<leader>lv", "<cmd>VimtexView<CR>", { desc = "VimTeX View" })
   end
 }
